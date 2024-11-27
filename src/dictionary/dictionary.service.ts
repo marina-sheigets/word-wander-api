9cd829bcffd289ee231c26a2cd468ce8d824a880
marina-sheigets <sejmar1705@gmail.com>
2024-11-27 22:54:46 +0200

    fix:  schema

--- src/dictionary/dictionary.service.ts
+++ src/dictionary/dictionary.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Dictionary } from './schemas/dictionary.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { AddWordDto } from './dto/add-word.dto';
 import { TrainingName } from 'src/constants/TrainingName';
 import { Training } from 'src/training/schemas/training.schema';
@@ -15,13 +15,16 @@ export class DictionaryService {
 
     public async addWord(request, { translation, word }: AddWordDto) {
         const userId = request.user.userId;
+
+        const wordInDictionary = await this.DictionaryModel.findOne({ user: userId, word });
+
         await this.DictionaryModel.updateOne(
             { user: userId, word },
             { $set: { translation } },
             { upsert: true }
         );
 
-        await this.addWordToAllTrainings(userId, word);
+        await this.addWordToAllTrainings(userId, wordInDictionary?._id as mongoose.Types.ObjectId);
     }
 
     public async getWords(request) {
@@ -33,13 +36,13 @@ export class DictionaryService {
         return this.DictionaryModel.deleteOne({ _id: wordId, user: request.user.userId });
     }
 
-    private async addWordToAllTrainings(userId: string, word: string) {
+    private async addWordToAllTrainings(userId: string, wordId: mongoose.Types.ObjectId) {
         const trainings = Object.values(TrainingName);
 
         for (const training of trainings) {
             await this.TrainingModel.updateOne(
-                { user: userId, name: training },
-                { $addToSet: { wordsIds: word } },
+                { user: new mongoose.Types.ObjectId(userId), name: training },
+                { $addToSet: { wordsIds: new mongoose.Types.ObjectId(wordId) } },
                 { upsert: true }
             );
         }
