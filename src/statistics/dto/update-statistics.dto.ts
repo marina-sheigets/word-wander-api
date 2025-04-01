6560697c9c79c210335ffe5f6109601e2cba86d4
marina-sheigets <sejmar1705@gmail.com>
2025-04-01 21:21:41 +0300

    fix: fix dtos for swagger

--- src/statistics/dto/update-statistics.dto.ts
+++ src/statistics/dto/update-statistics.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsString } from 'class-validator';
 import { StatisticsPath } from 'src/constants/StatisticsPaths';
 
 export class UpdateStatisticsDto {
+    @ApiProperty()
     @IsEnum(StatisticsPath, { message: 'Invalid fieldPath' })
     fieldPath: StatisticsPath;
 
+    @ApiProperty()
     @IsString()
     accuracyRate?: number;
 }
--- src/training/dto/update-ratio.dto.ts
+++ src/training/dto/update-ratio.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsNumber } from "class-validator";
 import { TrainingName } from "src/constants/TrainingName";
 
 export class UpdateRatioDto {
+    @ApiProperty()
     @IsNumber()
     accuracyRate: number;
 
+    @ApiProperty()
     @IsEnum(TrainingName)
     trainingName: TrainingName;
 }
