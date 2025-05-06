337b10401c2c5cf35cc143ce1177033a635aed43
marina-sheigets <sejmar1705@gmail.com>
2025-05-06 21:29:29 +0300

    fix: dictionary collection schema

--- src/dictionary-collection/schemas/dictionary-collection.schema.ts
+++ src/dictionary-collection/schemas/dictionary-collection.schema.ts
@@ -3,11 +3,14 @@ import mongoose, { Document } from "mongoose";
 
 @Schema({ versionKey: false, timestamps: true })
 export class DictionaryCollection extends Document {
+    @Prop({ required: true, type: mongoose.Types.ObjectId, ref: 'User' })
+    user_id: mongoose.Types.ObjectId;
+
     @Prop({ required: true, type: mongoose.Types.ObjectId, ref: 'Collection' })
     collection_id: mongoose.Types.ObjectId;
 
     @Prop({ required: true, type: mongoose.Types.ObjectId, ref: 'Dictionary' })
-    dictionary: string;
+    dictionary_id: string;
 }
 
 export const DictionaryCollectionSchema = SchemaFactory.createForClass(DictionaryCollection);
